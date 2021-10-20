1c7c963e7ac1c1fe14862cccd7a594da1e2f0a3f
Hussein <hessenaladawy@gmail.com>
2021-10-20 11:59:50 +0200

    Dependecies

--- InvoiceMangementApp.Api/Startup.cs
+++ InvoiceMangementApp.Api/Startup.cs
@@ -1,4 +1,4 @@
-using InvoiceMangementApp.Api.Data;
+using InvoiceMangementApp.Infrastrucure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
